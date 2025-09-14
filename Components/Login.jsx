cf1a0e83c2c7721b66ec13a7cb47b416b596a22b
Aiza Humayun <123739993+aizahumayun@users.noreply.github.com>
2025-09-14 15:07:40 -0700

    change

--- Components/Login.jsx
+++ Components/Login.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    div>div+input*2+p
-  )
-}
+    <div className="bg-cyan-500 w-50 h-80 flex h-screen items-center justify-center">
+      <div className=" w-40 h bg-amber-500  ">
+        <input type="text" />
+        <input type="text" />
+        <p></p>
+        <button></button>
+        <a href=""></a>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
