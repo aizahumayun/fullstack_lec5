9d5329805dbd02728d9014d0b34feeb0e4c00bd9
Aiza Humayun <123739993+aizahumayun@users.noreply.github.com>
2025-09-14 14:25:26 -0700

    changes

--- App.jsx
+++ App.jsx
@@ -1,4 +1,5 @@
 import Bangladesh from "./Components/Bangladesh";
+import Login from "./Components/Login";
 import Panama from "./Components/Panama";
 import Uae from "./Components/Uae";
 
@@ -6,11 +7,7 @@ function App() {
 
   return (
     <>
-    <Panama/>
-    <br />
-    <Uae/>
-    <br />
-    <Bangladesh />
+       <Login/>
     </>
   );
 }
---
+++ Components/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    div>div+input*2+p
+  )
+}
+
+export default Login
